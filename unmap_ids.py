c28c96c365aeec5322f77ce5ed0a1c9203509f9b
Ian Walton <iwalton3@gmail.com>
2018-12-27 19:59:42 -0500

    Add unmap tool for fixing files copied from container filesystems.

---
+++ unmap_ids.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+print("This will change owners all files in the current directory!")
+print("This can fix the uid/gid of files copied from containers.")
+confirm = input("Continue? [y/N] ")
+if confirm not in ("y","Y","yes","Yes"):
+    sys.exit(1)
+
+def unmap_ids(path):
+    if os.path.islink(path):
+        return
+    statres = os.stat(path)
+    newgid = statres.st_gid%100000
+    newuid = statres.st_uid%100000
+    if statres.st_gid != newgid or statres.st_uid != newuid:
+        print("%s:%s -> %s:%s : %s" % (statres.st_uid, statres.st_gid, newuid, newgid, path))
+        os.chown(path, newuid, newgid)
+
+for item in os.walk("."):
+    dir, subdirs, files = item
+    unmap_ids(dir)
+    for partial in files:
+        unmap_ids(os.path.join(dir,partial))
